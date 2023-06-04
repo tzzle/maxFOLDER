88fa8fc64fbe2d110eeebc95092e2406a2a57f3f
tzzle <130739462+tzzle@users.noreply.github.com>
2023-06-04 14:56:37 +0300

    Add files via upload

---
+++ 1/box-2/index.js
@@ -0,0 +1,216 @@
+function changeBackgroundColor() {
+    var colors = ["#AFD3E2", "#19A7CE", "#146C94", "#E06469"]; // Specify the colors you want to cycle through
+    var index = 0;
+    var div = document.getElementById("myDiv");
+
+    setInterval(function() {
+      div.style.backgroundColor = colors[index];
+      index = (index + 1) % colors.length;
+    }, 1000); // Change color every 1000 milliseconds (1 second)
+
+    var colorsOne = ['#E06469', '#F2B6A0', '#DEDEA7', '#FFF8D6'];
+    var indexOne = 0;
+    var divOne = document.getElementById('myDivOne');
+
+    setInterval(function() {
+        divOne.style.backgroundColor = colorsOne[indexOne];
+        indexOne = (indexOne + 1) % colorsOne.length;
+    }, 1000);
+
+    /*TWO*/
+
+    var colorsTwo = ['#99627A', '#C88EA7', '#E7CBCB', '#4C4C6D'];
+    var indexTwo = 0;
+    var divTwo = document.getElementById('myDivTwo');
+
+    setInterval(function() {
+        divTwo.style.backgroundColor = colorsTwo[indexTwo];
+        indexTwo = (indexTwo + 1) % colorsTwo.length;
+    }, 1000);
+
+    /*THREE*/
+
+    var colorsThree = ['#1B9C85', '#E8F6EF', '#FFE194', '#FEA1A1'];
+    var indexThree = 0;
+    var divThree = document.getElementById('myDivThree');
+
+    setInterval(function() {
+        divThree.style.backgroundColor = colorsThree[indexThree];
+        indexThree = (indexThree + 1) % colorsThree.length;
+    }, 1000);
+
+    /*FOUR*/
+
+    var colorsFour = ['#CBB279', '#E1D4BB', '#EEEEEE', '#FFFAF4'];
+    var indexFour = 0;
+    var divFour = document.getElementById('myDivFour');
+
+    setInterval(function() {
+        divFour.style.backgroundColor = colorsFour[indexFour];
+        indexFour = (indexFour + 1) % colorsFour.length;
+    }, 1000);
+
+    /*FIVE*/
+
+    var colorsFive = ['#FFFAF4', '#D25380', '#E08E6D', '#F6C391'];
+    var indexFive = 0;
+    var divFive = document.getElementById('myDivFive');
+
+    setInterval(function() {
+        divFive.style.backgroundColor = colorsFive[indexFive];
+        indexFive = (indexFive + 1) % colorsFive.length;
+    }, 1000);
+
+    /*SIX*/
+    var colorsSix = ['#FFD95A', '#C07F00', '#4C3D3D', '#393646'];
+    var indexSix = 0;
+    var divSix = document.getElementById('myDivSix');
+
+    setInterval(function() {
+        divSix.style.backgroundColor = colorsSix[indexSix];
+        indexSix = (indexSix + 1) % colorsSix.length;
+    }, 1000);
+
+    /*SEVEN*/
+    var colorsSeven = ['#6D5D6E', '#F4EEE0', '#454545', '#FF6000'];
+    var indexSeven = 0;
+    var divSeven = document.getElementById('myDivSeven');
+
+    setInterval(function() {
+        divSeven.style.backgroundColor = colorsSeven[indexSeven];
+        indexSeven = (indexSeven + 1) % colorsSeven.length;
+    }, 1000);
+
+    /*EIGHT*/
+    var colorsEight = ['#FFE6C7', '#000000', '#262A56', '#B8621B'];
+    var indexEight = 0;
+    var divEight = document.getElementById('myDivEight');
+
+    setInterval(function() {
+        divEight.style.backgroundColor = colorsEight[indexEight];
+        indexEight = (indexEight + 1) % colorsEight.length;
+    }, 1000);
+
+    /*NINE*/
+    var colorsNine = ['#FFF4E0', '#FFBF9B', '#B46060', '#4D4D4D'];
+    var indexNine = 0;
+    var divNine = document.getElementById('myDivNine');
+
+    setInterval(function() {
+        divNine.style.backgroundColor = colorsNine[indexNine];
+        indexNine = (indexNine + 1) % colorsNine.length;
+    }, 1000);
+
+    /*TEN*/
+    var colorsTen = ['#19A7CE', '#146C94', '#000000', '#2D2727'];
+    var indexTen = 0;
+    var divTen = document.getElementById('myDivTen');
+
+    setInterval(function() {
+        divTen.style.backgroundColor = colorsTen[indexTen];
+        indexTen = (indexTen + 1) % colorsTen.length;
+    }, 1000);
+
+    /*ELEVEN*/
+    var colorsEleven = ['#8F43EE', '#F0EB8D', '#2C3333', '#2E4F4F'];
+    var indexEleven = 0;
+    var divEleven = document.getElementById('myDivEleven');
+
+    setInterval(function() {
+        divEleven.style.backgroundColor = colorsEleven[indexEleven];
+        indexEleven = (indexEleven + 1) % colorsEleven.length;
+    }, 1000);
+
+    /*TWELVE*/
+    var colorsTwelve = ['#CBE4DE', '#4E6E81', '#F9DBBB', '#FF0303'];
+    var indexTwelve = 0;
+    var divTwelve = document.getElementById('myDivTwelve');
+
+    setInterval(function() {
+        divTwelve.style.backgroundColor = colorsTwelve[indexTwelve];
+        indexTwelve = (indexTwelve + 1) % colorsTwelve.length;
+    }, 1000);
+
+    /*THIRTEEN*/
+    var colorsThirteen = ['#865DFF', '#E384FF', '#FFA3FD', '#4D455D'];
+    var indexThirteen = 0;
+    var divThirteen = document.getElementById('myDivThirteen');
+
+    setInterval(function() {
+        divThirteen.style.backgroundColor = colorsThirteen[indexThirteen];
+        indexThirteen = (indexThirteen + 1) % colorsThirteen.length;
+    }, 1000);
+
+    /*FOURTEEN*/
+    var colorsFourteen = ['#F5E9CF', '#7DB9B6', '#7DB9B6', '#635985'];
+    var indexFourteen = 0;
+    var divFourteen = document.getElementById('myDivFourteen');
+
+    setInterval(function() {
+        divFourteen.style.backgroundColor = colorsFourteen[indexFourteen];
+        indexFourteen = (indexFourteen + 1) % colorsFourteen.length;
+    }, 1000);
+
+    /*FIFTEEN*/
+    var colorsFifteen = ['#393053', '#18122B', '#181823', '537FE7'];
+    var indexFifteen = 0;
+    var divFifteen = document.getElementById('myDivFifteen');
+
+    setInterval(function() {
+        divFifteen.style.backgroundColor = colorsFifteen[indexFifteen];
+        indexFifteen = (indexFifteen + 1) % colorsFifteen.length;
+    }, 1000);
+
+    /*SIXTEEN*/
+    var colorsSixteen = ['#2E3840', '#191825', '#865DFF', '#E384FF'];
+    var indexSixteen = 0;
+    var divSixteen = document.getElementById('myDivSixteen');
+
+    setInterval(function() {
+        divSixteen.style.backgroundColor = colorsSixteen[indexSixteen];
+        indexSixteen = (indexSixteen + 1) % colorsSixteen.length;
+    }, 1000);
+
+    /*SEVENTEEN*/
+    var colorsSeventeen = ['#E96479', '#F5E9CF', '#7DB9B6', '#635985'];
+    var indexSeventeen = 0;
+    var divSeventeen = document.getElementById('myDivSeventeen');
+
+    setInterval(function() {
+        divSeventeen.style.backgroundColor = colorsSeventeen[indexSeventeen];
+        indexSeventeen = (indexSeventeen + 1) % colorsSeventeen.length;
+    }, 1000);
+
+    /*EIGHTEEN*/
+    var colorsEighteen = ['#393053', '#18122B', '#181823', '#537FE7'];
+    var indexEighteen = 0;
+    var divEighteen = document.getElementById('myDivEighteen');
+
+    setInterval(function() {
+        divEighteen.style.backgroundColor = colorsEighteen[indexEighteen];
+        indexEighteen = (indexEighteen + 1) % colorsEighteen.length;
+    }, 1000);
+
+    /*NINETEEN*/
+    var colorsNineteen = ['#E9F8F9', '#F0EEED', '#609EA2', '#C92C6D'];
+    var indexNineteen = 0;
+    var divNineteen = document.getElementById('myDivNineteen');
+
+    setInterval(function() {
+        divNineteen.style.backgroundColor = colorsNineteen[indexNineteen];
+        indexNineteen = (indexNineteen + 1) % colorsNineteen.length; 
+    }, 1000);
+
+    /*TWENTY*/
+    var colorsTwenty = ['#000000', '#0F6292', '#16FF00', '#FFED00'];
+    var indexTwenty = 0;
+    var divTwenty = document.getElementById('myDivTwenty');
+
+    setInterval(function() {
+        divTwenty.style.backgroundColor = colorsTwenty[indexTwenty];
+        indexTwenty = (indexTwenty + 1) % colorsTwenty.length;
+    }, 1000);
+
+  }
+
+  changeBackgroundColor();
